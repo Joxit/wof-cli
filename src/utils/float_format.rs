eae4154e517f4beeb8dcdad3392be14738b57358
Joxit <joxit972@gmail.com>
2020-04-06 21:48:20 +0200

    feat(float_format): add force parameter

--- src/utils/float_format.rs
+++ src/utils/float_format.rs
@@ -1,5 +1,5 @@
 pub trait FloatFormat {
-  fn fmt_with_decimal(self) -> String;
+  fn fmt_with_decimal(self, force: bool) -> String;
 }
 
 fn trailing_zeros(n: i16) -> String {
@@ -11,15 +11,17 @@ fn trailing_zeros(n: i16) -> String {
 }
 
 impl FloatFormat for (bool, u64, i16) {
-  fn fmt_with_decimal(self) -> String {
+  fn fmt_with_decimal(self, force: bool) -> String {
     let (positive, mantissa, exponent) = self;
     let natural: u64 = ((mantissa as f64) * 10_f64.powi(exponent as i32)) as u64;
     let decimal = if exponent < 0 {
       let decimal = format!("{}", (mantissa - (natural * 10_u64.pow(-exponent as u32))));
       let zeros = trailing_zeros(exponent.abs() - (decimal.len() as i16));
       format!(".{}{}", zeros, decimal)
-    } else {
+    } else if force {
       String::from(".0")
+    } else {
+      String::new()
     };
     let sign = if positive { "" } else { "-" };
 
@@ -31,12 +33,22 @@ impl FloatFormat for (bool, u64, i16) {
 mod test {
   use super::*;
   #[test]
+  pub fn num_parts_force() {
+    assert_eq!((true, 0, 0).fmt_with_decimal(true), "0.0");
+    assert_eq!((true, 123456, -3).fmt_with_decimal(true), "123.456");
+    assert_eq!((false, 987654, -3).fmt_with_decimal(true), "-987.654");
+    assert_eq!((true, 1, -8).fmt_with_decimal(true), "0.00000001");
+    assert_eq!((false, 1, -8).fmt_with_decimal(true), "-0.00000001");
+    assert_eq!((true, 1, 2).fmt_with_decimal(true), "100.0");
+  }
+  #[test]
   pub fn num_parts() {
-    assert_eq!((true, 0, 0).fmt_with_decimal(), "0.0");
-    assert_eq!((true, 123456, -3).fmt_with_decimal(), "123.456");
-    assert_eq!((false, 987654, -3).fmt_with_decimal(), "-987.654");
-    assert_eq!((true, 1, -8).fmt_with_decimal(), "0.00000001");
-    assert_eq!((false, 1, -8).fmt_with_decimal(), "-0.00000001");
+    assert_eq!((true, 0, 0).fmt_with_decimal(false), "0");
+    assert_eq!((true, 123456, -3).fmt_with_decimal(false), "123.456");
+    assert_eq!((false, 987654, -3).fmt_with_decimal(false), "-987.654");
+    assert_eq!((true, 1, -8).fmt_with_decimal(false), "0.00000001");
+    assert_eq!((false, 1, -8).fmt_with_decimal(false), "-0.00000001");
+    assert_eq!((true, 1, 2).fmt_with_decimal(false), "100");
   }
 
   #[test]
