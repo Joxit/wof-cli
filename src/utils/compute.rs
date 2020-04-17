66351c863a0aa92555c97dc130d4c84a4fd0f15c
Joxit <joxit972@gmail.com>
2020-04-18 00:03:47 +0200

    feat(compute): add centroid compute

--- src/utils/compute.rs
+++ src/utils/compute.rs
@@ -7,6 +7,7 @@ pub trait GeoCompute {
   fn compute_area(&self) -> f64;
   fn compute_bbox(&self) -> Vec<f64>;
   fn compute_md5(&self) -> String;
+  fn compute_centroid(&self) -> (f64, f64);
 
   fn compute_bbox_string(&self) -> String {
     let bbox = self.compute_bbox();
@@ -38,6 +39,18 @@ fn compute_area_geojson_polygon(polygon: Vec<Vec<Vec<f64>>>) -> f64 {
   area
 }
 
+#[inline]
+fn compute_centroid_polyline(polyline: &Vec<Vec<f64>>) -> (f64, f64, i64) {
+  let mut x = 0.;
+  let mut y = 0.;
+  let len = polyline.len() - 1;
+  for i in 0..len {
+    x += polyline[i][0];
+    y += polyline[i][1];
+  }
+  (x, y, len as i64)
+}
+
 impl GeoCompute for Vec<f64> {
   fn compute_area(&self) -> f64 {
     0.0
@@ -45,6 +58,10 @@ impl GeoCompute for Vec<f64> {
 
   fn compute_bbox(&self) -> Vec<f64> {
     vec![self[0], self[1], self[0], self[1]]
+  }
+
+  fn compute_centroid(&self) -> (f64, f64) {
+    (self[0], self[1])
   }
 
   fn compute_md5(&self) -> String {
@@ -68,6 +85,11 @@ impl GeoCompute for Vec<Vec<f64>> {
         bbox[3].max(pts[1]),
       ]
     })
+  }
+
+  fn compute_centroid(&self) -> (f64, f64) {
+    let (x, y, len) = compute_centroid_polyline(self);
+    (x / (len as f64), y / (len as f64))
   }
 
   fn compute_md5(&self) -> String {
@@ -109,6 +131,7 @@ impl<'a> GeoCompute for crate::WOFGeoJSON<'a> {
     }
     0.
   }
+
   fn compute_bbox(&self) -> Vec<f64> {
     let geom_type = match self.geometry.get("type") {
       Some(v) => v.as_str(),
@@ -159,6 +182,57 @@ impl<'a> GeoCompute for crate::WOFGeoJSON<'a> {
     vec![0., 0., 0., 0.]
   }
 
+  fn compute_centroid(&self) -> (f64, f64) {
+    let geom_type = match self.geometry.get("type") {
+      Some(v) => v.as_str(),
+      _ => return (0., 0.),
+    };
+    let coords = match self.geometry.get("coordinates") {
+      Some(c) => c,
+      _ => return (0., 0.),
+    };
+    match geom_type {
+      Some("Point") => {
+        if let Some(point) = coords.as_geom_point() {
+          return point.compute_centroid();
+        }
+      }
+      Some("MultiPoint") => {
+        if let Some(multi_point) = coords.as_geom_multi_point() {
+          return multi_point.compute_centroid();
+        }
+      }
+      Some("LineString") => {
+        if let Some(line) = coords.as_geom_line() {
+          return line.compute_centroid();
+        }
+      }
+      Some("Polygon") => {
+        if let Some(polygon) = coords.as_geom_polygon() {
+          let (x, y, len) = polygon.iter().fold((0., 0., 0), |pacc, part| {
+            let compute = compute_centroid_polyline(part);
+            (pacc.0 + compute.0, pacc.1 + compute.1, pacc.2 + compute.2)
+          });
+          return (x / (len as f64), y / (len as f64));
+        }
+      }
+      Some("MultiPolygon") => {
+        if let Some(multi_polygon) = coords.as_geom_multi_polygon() {
+          let (x, y, len) = multi_polygon.iter().fold((0., 0., 0), |acc, polygon| {
+            let fold = polygon.iter().fold((0., 0., 0), |pacc, part| {
+              let compute = compute_centroid_polyline(part);
+              (pacc.0 + compute.0, pacc.1 + compute.1, pacc.2 + compute.2)
+            });
+            (acc.0 + fold.0, acc.1 + fold.1, acc.2 + fold.2)
+          });
+          return (x / (len as f64), y / (len as f64));
+        }
+      }
+      _ => {}
+    }
+    (0., 0.)
+  }
+
   fn compute_md5(&self) -> String {
     let mut result: Vec<u8> = vec![];
     crate::object_to_writer(self.geometry, &mut result).unwrap();
@@ -176,6 +250,8 @@ mod test {
     assert_eq!(point.compute_area(), 0.0);
     assert_eq!(point.compute_bbox(), vec![-71.0, 41.0, -71.0, 41.0]);
     assert_eq!(point.compute_bbox_string(), "-71.0,41.0,-71.0,41.0");
+    assert_eq!(point.compute_centroid(), (-71.0, 41.0));
+    // assert_eq!(point.compute_center_of_mass(), (-71.0, 41.0));
   }
 
   #[test]
@@ -190,10 +266,12 @@ mod test {
     assert_eq!(polygon.compute_area(), 287.5);
     assert_eq!(polygon.compute_bbox(), vec![113.0, -27.0, 154.0, -15.0]);
     assert_eq!(polygon.compute_bbox_string(), "113.0,-27.0,154.0,-15.0");
+    assert_eq!(polygon.compute_centroid(), (134.0, -19.75));
+    // assert_eq!(polygon.compute_center_of_mass(), (134.764058, -20.408116));
   }
 
   #[test]
-  pub fn geojson() {
+  pub fn polygon_geojson() {
     let json = json::object! {
       "type" => "Feature",
       "properties" => json::object!{},
@@ -217,8 +295,53 @@ mod test {
     // assert_eq!(wof_obj.compute_area_m(), 3332714287168.220703);
     assert_eq!(wof_obj.compute_bbox(), vec![113.0, -27.0, 154.0, -15.0]);
     assert_eq!(wof_obj.compute_bbox_string(), "113.0,-27.0,154.0,-15.0");
-    // assert_eq!(wof_obj.compute_latitude(), -20.408116);
-    // assert_eq!(wof_obj.compute_longitude(), -20.408116);
+    assert_eq!(wof_obj.compute_centroid(), (134.0, -19.75));
+    // assert_eq!(wof_obj.compute_center_of_mass(), (134.764058, -20.408116));
     assert_eq!(wof_obj.compute_md5(), "1d113db66a333671083cf93919ed85b9");
+  }
+
+  #[test]
+  pub fn multi_polygon_geojson() {
+    let json = json::object! {
+      "type" => "Feature",
+      "properties" => json::object!{},
+      "geometry" => json::object!{
+        "coordinates" => vec![
+          vec![vec![
+            vec![102.0, 2.0],
+            vec![103.0, 2.0],
+            vec![103.0, 3.0],
+            vec![102.0, 3.0],
+            vec![102.0, 2.0]]
+          ],
+          vec![vec![
+            vec![100.0, 0.0],
+            vec![101.0, 0.0],
+            vec![101.0, 1.0],
+            vec![100.0, 1.0],
+            vec![100.0, 0.0]],
+          vec![vec![100.2, 0.2],
+            vec![100.8, 0.2],
+            vec![100.8, 0.8],
+            vec![100.2, 0.8],
+            vec![100.2, 0.2]]
+          ]
+        ],
+        "type" => "MultiPolygon"
+      },
+      "bbox" => vec![113.0, -27.0, 154.0, -15.0],
+      "id" => 0,
+    };
+    let wof_obj = crate::WOFGeoJSON::as_valid_wof_geojson(&json).unwrap();
+    assert_eq!(wof_obj.compute_area(), 1.6400000000000035);
+    // assert_eq!(wof_obj.compute_area_m(), 20266558929.082764);
+    assert_eq!(wof_obj.compute_bbox(), vec![100.0, 0.0, 103.0, 3.0]);
+    assert_eq!(wof_obj.compute_bbox_string(), "100.0,0.0,103.0,3.0");
+    assert_eq!(
+      wof_obj.compute_centroid(),
+      (101.16666666666667, 1.1666666666666667)
+    );
+    // assert_eq!(wof_obj.compute_center_of_mass(), (101.719512, 1.719512));
+    assert_eq!(wof_obj.compute_md5(), "e965f294d0c0a5fe9e42a51285edbabd");
   }
 }
