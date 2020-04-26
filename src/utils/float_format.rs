f50f0f35dcf3d583fd3a39818c390417f50c4592
Joxit <joxit972@gmail.com>
2020-04-26 17:00:00 +0200

    feat(with_precision): add `with_precision` to `FloatFormat`

--- src/utils/float_format.rs
+++ src/utils/float_format.rs
@@ -1,5 +1,8 @@
+use json::{number::Number, JsonValue};
+
 pub trait FloatFormat {
   fn fmt_with_decimal(self, force: bool) -> String;
+  fn with_precision(self, precision: i16) -> Self;
 }
 
 fn trailing_zeros(n: i16) -> String {
@@ -27,13 +30,34 @@ impl FloatFormat for (bool, u64, i16) {
 
     format!("{}{}{}", sign, natural, decimal)
   }
+
+  fn with_precision(self, precision: i16) -> Self {
+    let (positive, mantissa, exponent) = self;
+    if exponent < -precision {
+      let diff_exponent = precision + exponent;
+      let new_mantissa = ((mantissa as f64) * 10_f64.powi(diff_exponent as i32)).round() as u64;
+      if new_mantissa == 0 {
+        (true, 0, 0)
+      } else {
+        (positive, new_mantissa, -precision)
+      }
+    } else {
+      self
+    }
+  }
 }
 
 impl FloatFormat for f64 {
   fn fmt_with_decimal(self, force: bool) -> String {
-    json::number::Number::from(self)
-      .as_parts()
-      .fmt_with_decimal(force)
+    Number::from(self).as_parts().fmt_with_decimal(force)
+  }
+
+  fn with_precision(self, precision: i16) -> Self {
+    let (positive, mantissa, exponent) = Number::from(self).as_parts().with_precision(precision);
+
+    JsonValue::Number(Number::from_parts(positive, mantissa, exponent))
+      .as_f64()
+      .unwrap()
   }
 }
 
@@ -86,5 +110,31 @@ mod test {
     assert_eq!(super::trailing_zeros(1), String::from("0"));
     assert_eq!(super::trailing_zeros(10), String::from("0000000000"));
     assert_eq!(super::trailing_zeros(-10), String::from(""));
+  }
+
+  #[test]
+  pub fn parts_with_precision() {
+    assert_eq!((true, 0, 0).with_precision(6), (true, 0, 0));
+    assert_eq!((true, 123456, -3).with_precision(6), (true, 123456, -3));
+    assert_eq!((false, 987654, -3).with_precision(6), (false, 987654, -3));
+    assert_eq!((true, 1, -8).with_precision(6), (true, 0, 0));
+    assert_eq!((false, 1, -8).with_precision(8), (false, 1, -8));
+    assert_eq!((false, 1, -8).with_precision(6), (true, 0, 0));
+    assert_eq!((true, 1, 2).with_precision(6), (true, 1, 2));
+    assert_eq!((true, 12345, -8).with_precision(6), (true, 123, -6));
+    assert_eq!((true, 12345, -7).with_precision(6), (true, 1235, -6));
+  }
+
+  #[test]
+  pub fn f64_with_precision() {
+    assert_eq!((0.0).with_precision(6), 0.0);
+    assert_eq!((123.456).with_precision(6), 123.456);
+    assert_eq!((-987.654).with_precision(6), -987.654);
+    assert_eq!((0.00000001).with_precision(6), 0.0);
+    assert_eq!((-0.00000001).with_precision(8), -0.00000001);
+    assert_eq!((-0.00000001).with_precision(6), 0.0);
+    assert_eq!((100.0).with_precision(6), 100.0);
+    assert_eq!((0.00012345).with_precision(6), 0.000123);
+    assert_eq!((0.0012345).with_precision(6), 0.001235);
   }
 }
