f374351c06a1aea34dfb976f7e54bd1193acea1c
Joxit <joxit972@gmail.com>
2020-09-20 22:25:31 +0200

    feat(export): add default properties

--- src/export.rs
+++ src/export.rs
@@ -1,5 +1,5 @@
 use crate::utils::{GeoCompute, GeoJsonUtils, JsonUtils};
-use json::JsonValue;
+use json::{array, JsonValue};
 
 pub fn export_json_value(json: JsonValue) -> Result<JsonValue, String> {
   json.assert_is_object()?;
@@ -18,36 +18,76 @@ pub fn export_json_value(json: JsonValue) -> Result<JsonValue, String> {
 }
 
 fn export_porperties(json: &JsonValue) -> Result<JsonValue, String> {
-  let mut properties = JsonValue::new_object();
-
   let obj = json
     .as_object()
     .unwrap()
     .get("properties")
-    .ok_or(String::from(
-      "For WOF objects, `properties` key is required as an object.",
-    ))?;
+    .ok_or("`properties` key is required.")?;
 
-  Ok(properties)
+  let mut properties = obj
+    .as_object()
+    .ok_or("`properties` key must be an object")?
+    .clone();
+
+  if properties.get("edtf:cessation").is_none() {
+    properties.insert("edtf:cessation", JsonValue::from("uuuu"));
+  }
+  if properties.get("edtf:inception").is_none() {
+    properties.insert("edtf:inception", JsonValue::from("uuuu"));
+  }
+  if properties.get("iso:country").is_none() {
+    properties.insert("iso:country", JsonValue::from(""));
+  }
+  if properties.get("mz:hierarchy_label").is_none() {
+    properties.insert("mz:hierarchy_label", JsonValue::from(1));
+  }
+  if properties.get("mz:is_current").is_none() {
+    properties.insert("mz:is_current", JsonValue::from(-1));
+  }
+  if properties.get("src:geom").is_none() {
+    properties.insert("src:geom", JsonValue::from("unknown"));
+  }
+  if properties.get("wof:belongsto").is_none() {
+    properties.insert("wof:belongsto", array![]);
+  }
+  if properties.get("wof:breaches").is_none() {
+    properties.insert("wof:breaches", array![]);
+  }
+  if properties.get("wof:country").is_none() {
+    properties.insert("wof:country", JsonValue::from(""));
+  }
+  if properties.get("wof:hierarchy").is_none() {
+    properties.insert("wof:hierarchy", array![]);
+  }
+  if properties.get("wof:parent_id").is_none() {
+    properties.insert("wof:parent_id", JsonValue::from(-1));
+  }
+  if properties.get("wof:superseded_by").is_none() {
+    properties.insert("wof:superseded_by", array![]);
+  }
+  if properties.get("wof:supersedes").is_none() {
+    properties.insert("wof:supersedes", array![]);
+  }
+  if properties.get("wof:tags").is_none() {
+    properties.insert("wof:tags", array![]);
+  }
+
+  Ok(JsonValue::Object(properties))
 }
 
 fn export_geometry(json: &JsonValue) -> Result<JsonValue, String> {
-  let mut geometry = JsonValue::new_object();
-
   let obj = json
     .as_object()
     .unwrap()
     .get("geometry")
-    .ok_or(format!("`geometry` key is required."))?
+    .ok_or("`geometry` key is required.")?
     .as_object()
-    .ok_or(format!("`geometry` key must be an object.",))?;
+    .ok_or("`geometry` key must be an object.")?;
 
   let coordinates = obj
     .get("coordinates")
-    .ok_or(format!("`coordinates` key is required."))?;
-  let _type = obj
-    .get("type")
-    .ok_or(format!("In `geometry`, `type` is required"))?;
+    .ok_or("`coordinates` key is required.")?;
+  let _type = obj.get("type").ok_or("In `geometry`, `type` is required")?;
 
   if !coordinates.is_array() {
     return Err(format!("`coordinates` key must be an array."));
@@ -56,22 +96,22 @@ fn export_geometry(json: &JsonValue) -> Result<JsonValue, String> {
   let coords = match _type.as_str() {
     Some("Point") => json::array!(coordinates
       .as_geom_point()
-      .ok_or(format!("`coordinates` malformed for `type` Point."))?),
+      .ok_or("`coordinates` malformed for `type` Point.")?),
     Some("MultiPoint") => json::array!(coordinates
       .as_geom_multi_point()
-      .ok_or(format!("`coordinates` malformed for `type` MultiPoint."))?),
+      .ok_or("`coordinates` malformed for `type` MultiPoint.")?),
     Some("LineString") => json::array!(coordinates
       .as_geom_line()
-      .ok_or(format!("`coordinates` malformed for `type` LineString."))?),
-    Some("MultiLineString") => json::array!(coordinates.as_geom_multi_line().ok_or(format!(
-      "`coordinates` malformed for `type` MultiLineString."
-    ))?),
+      .ok_or("`coordinates` malformed for `type` LineString.")?),
+    Some("MultiLineString") => json::array!(coordinates
+      .as_geom_multi_line()
+      .ok_or("`coordinates` malformed for `type` MultiLineString.")?),
     Some("Polygon") => json::array!(coordinates
       .as_geom_polygon()
-      .ok_or(format!("`coordinates` malformed for `type` Polygon."))?),
+      .ok_or("`coordinates` malformed for `type` Polygon.")?),
     Some("MultiPolygon") => json::array!(coordinates
       .as_geom_multi_polygon()
-      .ok_or(format!("`coordinates` malformed for `type` MultiPolygon."))?),
+      .ok_or("`coordinates` malformed for `type` MultiPolygon.")?),
     Some(t) => return Err(format!("type {} is not supported.", t)),
     None => return Err(format!("In `geometry`, `type` key is required.")),
   };
